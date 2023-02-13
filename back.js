21edfc7ae767cff9c7e8492fd1432d9e85870822
TereseBo <terese.boderus@gmail.com>
2023-02-13 12:27:15 +0200

    -

--- back.js
+++ back.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const db= require("./data/db.json")
+const hem=require("index.html")
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     console.log(req)
-  res.send(db)
+  res.send(hem)
 })
 
 app.listen(port, () => {
